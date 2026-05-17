29171937526dfb3e77f2e619d82b8539f821f59c
clpi <5184578+clpi@users.noreply.github.com>
2026-05-17 00:48:48 +0000

    ⚡ Non-blocking yield in async ABI polling loop

--- src/util/env.zig
+++ src/util/env.zig
@@ -1,15 +1,15 @@
 const std = @import("std");
 
 pub fn hasEnvVarConstant(comptime name: []const u8) bool {
-    const name_z = std.heap.c_allocator.dupeZ(u8, name) catch return false;
+    const name_z = std.heap.c_allocator.dupe(u8, name) catch return false;
     defer std.heap.c_allocator.free(name_z);
-    return std.c.getenv(name_z.ptr) != null;
+    return std.posix.getenv(name_z.ptr) != null;
 }
 
 pub fn getEnvVarOwned(allocator: std.mem.Allocator, name: []const u8) ![]u8 {
-    const name_z = try allocator.dupeZ(u8, name);
+    const name_z = try allocator.dupe(u8, name);
     defer allocator.free(name_z);
 
-    const value = std.c.getenv(name_z.ptr) orelse return error.EnvironmentVariableNotFound;
+    const value = std.posix.getenv(name_z.ptr) orelse return error.EnvironmentVariableNotFound;
     return try allocator.dupe(u8, std.mem.span(value));
 }
